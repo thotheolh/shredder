1dac60ddc981f4ab68f047033f48e944ba93108f
Michael Rawson <michaelrawson76@gmail.com>
2011-10-22 16:02:15 +0100

    Zeroing and Removal options added

--- Shred.py
+++ Shred.py
@@ -1,11 +1,28 @@
 #!/usr/bin/python
 import os
-def shred(filename, iterations):
+def shred(filename, iterations, zero, remove):
 	#shredding function
-	command = "shred " + "-n" + iterations + " " + filename
+	command = "shred " + "-n" + iterations
+	if(zero == "y"):
+		command += " -z"
+	if(remove == "y"):
+		command += " -u"
+	command += " " + filename
 	os.system(command)
 
+
+
+#shred options input
 filename = raw_input("Enter file to shred:	")
 iterations = raw_input("Iterations:	")
+zero = raw_input("Zero after shredding? [y/n]")
+while ((zero != "y") and (zero != "n")):
+	zero = ""
+	zero = raw_input("Zero after shredding? [y/n]")
+remove = raw_input("Remove file after shredding[y/n]")
+while ((remove != "y") and (remove != "n")):
+	remove = ""
+	remove = raw_input("Zero after shredding? [y/n]")
+
 #shred call
-shred(filename, iterations)
+shred(filename, iterations, zero, remove)
