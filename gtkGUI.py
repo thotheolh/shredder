e4e2c800176b67fbc0f987d408f60baa834457b8
thotheolh <thotheolh@thotheolh-u.(none)>
2011-12-08 14:54:31 +0800

    Initial GUI design with widgets. Not workable yet.

--- gtkGUI.py
+++ gtkGUI.py
@@ -5,7 +5,9 @@ import sys
 importStatus = False
 
 try:
-	from gtk import *
+	import pygtk
+	pygtk.require('2.0')
+	import gtk
 	importStatus = True
 
 except ImportError:
@@ -18,10 +20,42 @@ if importStatus:
     class gtkGUI():
 
         def __init__(self):
-            print 'gtkGUI imported'
+            print "gtkGUI imported"
 
         def startGUI(self):
-            print 'GUI Started'
+            print "GUI Started"
+            self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+            self.window.set_border_width(10)
+            self.window.connect("destroy", self.destroy)
+
+			## Labels
+            self.filelbl = gtk.Label("File / Folder: ")
+            self.iterationlbl = gtk.Label("Shred Iterations: ")
+            self.zerolbl = gtk.Label("Zero-ing")
+
+			## Buttons
+            self.shredbtn = gtk.Button("Shred !")
+            self.filechoosebtn = gtk.Button("...")
+            self.trashbtn = gtk.Button("Trash")
+
+			## Check Boxes
+            self.zero = gtk.CheckButton(label=None)
+
+			## Text Fields
+            self.filenametf = gtk.Entry(max=0)
+            self.filenametf.set_max_length(20)
+            self.itertf = gtk.Entry(max=0)
+            self.itertf.set_max_length(8)
+
+            ## Text Area
+            ## self.status = gtk.
+
+			## Packing widgets into window
+            self.window.add(self.shredbtn)
+
+			## Presenting window
+            self.window.show_all()
+            gtk.main()
             return None
 
         def checkGtkVersion(self):
@@ -31,4 +65,5 @@ if importStatus:
         def checkPyGTKVersion(self):
             return gtk.pygtk_version
 
-
+        def destroy(self, widget, data=None):
+            return gtk.main_quit()
