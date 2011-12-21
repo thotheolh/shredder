4a718b1ef276cb8679b720ad3fed971328381c37
Thotheolh <twzgerald@gmail.com>
2011-12-21 09:01:36 +0800

    Fixed overly too big window icon and merged and edited LXDE support.

--- mainGUI.py
+++ mainGUI.py
@@ -5,17 +5,21 @@ from gtkGUI import *
 
 desktop_environment = 'generic'
 if os.environ.get('KDE_FULL_SESSION') == 'true':
-    desktop_environment = 'kde'
+    desktop_environment = 'KDE'
     print 'Desktop is ',desktop_environment
 elif os.environ.get('GNOME_DESKTOP_SESSION_ID'):
-    desktop_environment = 'gnome'
+    desktop_environment = 'Gnome'
+    print 'Desktop is ',desktop_environment
+    gtkGUI()
+elif os.environ.get('XDG_CURRENT_DESKTOP') == 'LXDE':
+    desktop_environment = 'LXDE'
     print 'Desktop is ',desktop_environment
     gtkGUI()
 else:
     try:
         info = getoutput('xprop -root _DT_SAVE_MODE')
         if ' = "xfce4"' in info:
-            desktop_environment = 'xfce'
+            desktop_environment = 'XFCE'
             print 'Desktop is ',desktop_environment
     except (OSError, RuntimeError):
         pass
